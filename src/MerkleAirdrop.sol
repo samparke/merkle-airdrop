97b65457d01964c4f7c4a68a83517017f759713d
Samuel Parke <samueljparke@gmail.com>
2025-08-11 19:34:31 +0100

    claim function

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,4 +1,33 @@
 //SPDX-Licence-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract MerkleAirdrop {}
+import {AirdropToken} from "./AirdropToken.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+
+    error MerkleAirdrop__InvalidProof();
+
+    address[] claimers;
+    IERC20 private immutable i_airdropToken;
+    bytes32 private immutable i_merkleRoot;
+
+    event Claim(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_airdropToken = airdropToken;
+        i_merkleRoot = merkleRoot;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // hash twice to avoid collusion
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
+}
