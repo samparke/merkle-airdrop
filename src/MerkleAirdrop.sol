a5a1a7fe2afeb2e9ae3edf2ec2d96b7217675beb
Samuel Parke <samueljparke@gmail.com>
2025-08-11 19:38:13 +0100

    added check for claim

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -9,10 +9,12 @@ contract MerkleAirdrop {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
     address[] claimers;
     IERC20 private immutable i_airdropToken;
     bytes32 private immutable i_merkleRoot;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
 
     event Claim(address account, uint256 amount);
 
@@ -22,11 +24,15 @@ contract MerkleAirdrop {
     }
 
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
         // hash twice to avoid collusion
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdrop__InvalidProof();
         }
+        s_hasClaimed[account] = true;
         emit Claim(account, amount);
         i_airdropToken.safeTransfer(account, amount);
     }
