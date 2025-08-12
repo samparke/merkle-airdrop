87e0d6f7ab037773f41683c50ae46757f460c61c
Samuel Parke <samueljparke@gmail.com>
2025-08-12 07:24:12 +0100

    test for claiming airdrop

---
+++ test/MerkleAirdrop.t.sol
@@ -0,0 +1,40 @@
+//SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {AirdropToken} from "../src/AirdropToken.sol";
+
+contract MerkleAirdropTest is Test {
+    MerkleAirdrop public airdrop;
+    AirdropToken public token;
+    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 AMOUNT_TO_CLAIM = 25 * 1e18;
+    uint256 AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+    address user;
+    uint256 userPrivKey;
+
+    function setUp() public {
+        token = new AirdropToken();
+        airdrop = new MerkleAirdrop(ROOT, token);
+        token.mint(token.owner(), AMOUNT_TO_SEND);
+        token.transfer(address(airdrop), AMOUNT_TO_SEND);
+        // this is so we can add the address to the input addresses we want to allow to claim
+        // once address is added to inputs, run forge script/script/GenerateInput.s.sol
+        // and then, forge script script/MakeMerkle.s.sol
+        console.log("user address", user);
+        (user, userPrivKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("ending balance", endingBalance);
+        assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
+    }
+}
